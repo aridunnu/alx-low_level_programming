4d12642cea304c51ec49a0f44b43614dde47c1db
aridunnu <fakeyekehinde17@gmail.com>
2022-03-11 22:36:04 +0000

    completed task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**                                                                                              
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 return (1);
 }
