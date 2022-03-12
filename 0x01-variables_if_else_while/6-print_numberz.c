394580d9952326f89bf43fc9508a10bc848c6b21
aridunnu <fakeyekehinde17@gmail.com>
2022-03-12 17:36:49 +0000

    completed task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints numbers 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
