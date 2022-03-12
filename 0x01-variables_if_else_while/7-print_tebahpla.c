36285c9b130f9192bd53a5af5f1108eab879947e
aridunnu <fakeyekehinde17@gmail.com>
2022-03-12 18:07:26 +0000

    completed task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabet in reverse.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+int i;
+
+for (i = 0; i < 26; i--)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
