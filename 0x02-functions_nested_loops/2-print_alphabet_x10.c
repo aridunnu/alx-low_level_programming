66b632840c0dbf7c709e947b7892e6d2b996087c
aridunnu <fakeyekehinde17@gmail.com>
2022-03-16 22:51:29 +0000

    completed task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - function that prints lower case alphabet
+ *
+ *Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
