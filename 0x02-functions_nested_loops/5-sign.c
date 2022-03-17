ab9bf6ad8b697ba1a0c2e6363f88373be9a5e871
aridunnu <fakeyekehinde17@gmail.com>
2022-03-17 00:28:16 +0000

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 if (n > 0)
 
 {
-putchar('+');
+putchar(43);
 return (1);
 }
 else if (n == 0)
 {
-putchar('0');
+putchar(48);
 return (0);
 }
 else
 {
-putchar('-');
+putchar(45);
 return (-1);
 }
 putchar('\n');
