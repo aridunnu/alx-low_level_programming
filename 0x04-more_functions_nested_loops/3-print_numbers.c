2dceefda16be513a68957a9d48809f60406fb086
aridunnu <fakeyekehinde17@gmail.com>
2022-03-17 20:08:24 +0000

    function that prints the numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
  */
 void print_numbers(void)
 {
-      
-	int i;
+int i;
 
-	for (i  = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
+for (i  = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
 }
