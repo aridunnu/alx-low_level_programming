4a71f6b6447878f817e0c50090ea519c13237dc3
aridunnu <fakeyekehinde17@gmail.com>
2022-03-17 00:21:02 +0000

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,8 @@
 /**
  * print_sign - prints the sign of a number n
  * @n: argument of a funtion
- * Return: Always 0 (Success)
+ * Return: 1 is greater than zero. 0 is zero.
+ * -1 is less than zero.
  */
 int print_sign(int n)
 {
