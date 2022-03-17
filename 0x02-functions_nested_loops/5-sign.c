a438e9d44c2a86ec3f1b1b5dd62359a7d08081f2
aridunnu <fakeyekehinde17@gmail.com>
2022-03-17 02:16:36 +0000

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,31 @@
 #include "main.h"
 #include <stdio.h>
-#include <time.h>
 
 /**
- * print_sign - prints the sign of a number n
- * @n: argument of a funtion
+ * print_sign - Determines if the input number
+ * greater, iqual or less than zero.
+ *
+ * @n: The input number as an integer.
+ *
  * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
 int print_sign(int n)
 {
-
-
-if (n > 0)
-{
-return (1);
-putchar('+');
-}
-else if (n == 0)
-{
-return (0);
-putchar('0');
-}
-else
-{
-return (-1);
-putchar('-');
-}
-putchar('\n');
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
