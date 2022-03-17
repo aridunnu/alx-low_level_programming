966356e6871ecaa21b1218727a394edd1961e213
aridunnu <fakeyekehinde17@gmail.com>
2022-03-17 21:25:35 +0000

    updated fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,9 +10,9 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
