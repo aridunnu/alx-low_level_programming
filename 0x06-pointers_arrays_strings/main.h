c17eefa05c968bd305eb929faf8a36c785b01c5a
aridunnu <fakeyekehinde17@gmail.com>
2022-03-25 21:59:01 +0000

    function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,5 +18,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+char *leet(char *);
+int _putchar(char c);
+	     
 #endif /* _SAMPLE_HEADER_H_ */
