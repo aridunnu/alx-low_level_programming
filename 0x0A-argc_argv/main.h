9303ab5eb5e3f926699b80226410dbb438dd3840
aridunnu <fakeyekehinde17@gmail.com>
2022-04-05 09:57:03 +0000

    program that prints its name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+
+#endif /* _SAMPLE_HEADER_H_ */
